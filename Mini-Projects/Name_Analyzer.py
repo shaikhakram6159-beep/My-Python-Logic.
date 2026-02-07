ef323f1bbdf90c2864f78b996f20f82b030841c2
shaikhakram6159-beep <shaikhakram6159@gmail.com>
2026-02-07 15:38:21 +0530

    Add: First Mini-Project (Name Analyzer)
    Added a standalone project that demonstrates String manipulation. This includes:
    
    - Input handling and user greeting. - Calculating string length. - Positive and Negative indexing to find characters. - Using the find() method for space detection. - Clean code with professional comments for better readability.

---
+++ Mini-Projects/Name_Analyzer.py
@@ -0,0 +1,5 @@
+name=input("Enter your name :- ")
+print("your names len is",len(name))#printing the length of the name
+print("first letter is ",name[0])#printing the first letter of the name
+print("last letter is",name[-1])#printing the last letter of the name
+print (name.find(" "))#printing the space in the name
